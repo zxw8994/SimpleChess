e6f9f59b00889aa7523e0ac1e2a9e802b28778c6
Zachary Wilken <zxw8994@rit.edu>
2018-07-22 16:32:25 -0400

    Can toggle instructions

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,6 +7,16 @@ public class UIManager : MonoBehaviour {
 
     public GameObject gameOverScreen;
     public GameManager gm;
+    public GameObject Instructions;
+    bool instructionsActive = true;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("p"))
+        {
+            ActivateInstruction();
+        }
+    }
 
     public void GameOver()
     {
@@ -22,5 +32,18 @@ public class UIManager : MonoBehaviour {
     public void ExitGame()
     {
         gm.ExitGame();
+    }
+
+    public void ActivateInstruction()
+    {
+        if (instructionsActive)
+        {
+            Instructions.SetActive(false);
+            instructionsActive = false;
+        }
+        else {
+            Instructions.SetActive(true);
+            instructionsActive = true;
+        }
     }
 }
